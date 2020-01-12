05d76353d97df8fcc6ec4db4d8ffaefe427c7d10
Gernot <58399790+gheisenberg@users.noreply.github.com>
2020-01-12 18:33:14 +0100

    call of the new DeviceFactory (still a problem with making the objects globally available)

--- AdAnalyzerSoftware/PrepareAction.m
+++ AdAnalyzerSoftware/PrepareAction.m
@@ -1,19 +1,36 @@
 classdef PrepareAction
-    %PREPAREACTION Summary of this class goes here
-    %   Detailed explanation goes here
+    % PrepareAction Summary of this class goes here
+    % 
+    % PrepareAction creates three Objects: 
+    %   - ConfigManager : load, save and validate _Config_ objects
+    %   - DataFactory   : parses StimulusInterval definitions
+    %   - DeviceFactory : pares device config files and sets global
+    %                     parameters
+    %
     
     properties
         configManager = ConfigManager(); 
         dataFactory = DataFactory();
+        deviceFactory = DeviceFactory();
     end
     
     methods
         function data = prepare(self,conf)
+            % validate first and if OK then create the corresponding data
             isValid = self.configManager.validate(conf);
             if (isValid)
                 data  = self.dataFactory.createData(conf);
                 data.isValid = 1; 
             end
+            % parse the device files and create those
+            eegDevice = self.deviceFactory.createEEGDevice(conf);
+            edaDevice = self.deviceFactory.createEDADevice(conf);
+            hrvDevice = self.deviceFactory.createHRVDevice(conf);
+            
+            % this works - but I cannot pass them out as global vars :-(
+            %disp(eegDevice.electrodePositions(1));
+            %disp(edaDevice.samplingRate);
+            %disp(hrvDevice.samplingRate);
         end
     end
     
